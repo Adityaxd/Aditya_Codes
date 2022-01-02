ff436d1ba0dc03420a8a5dcb98ac218a15ef2027
Aditya Chaudhary <achaudhary.chaudhary991@gmail.com>
2022-01-02 15:54:21 +0530

    Time: 17 ms (11.45%), Space: 15.6 MB (83.92%) - LeetHub

--- trapping-rain-water/trapping-rain-water.cpp
+++ trapping-rain-water/trapping-rain-water.cpp
@@ -5,14 +5,21 @@ public:
         int sz = height.size(), leftMax = 0, rightMax = 0, left = 0, res = 0, right = sz - 1;
         
         while(left <= right) {
+            
             if(height[left] <= height[right]) {
-                if(height[left] >= leftMax) leftMax = height[left]; 
-                else res += leftMax - height[left];
+                // if(height[left] >= leftMax) leftMax = height[left]; 
+                leftMax = max(leftMax, height[left]);
+                //if current height is lesser than the left maximum height, then we can store water, here we are storing water.
+                // else
+                res += leftMax - height[left]; 
                 left++;
             }
             else {
-                if(height[right] >= rightMax) rightMax = height[right];
-                else res += rightMax - height[right];
+                // if(height[right] >= rightMax) rightMax = height[right];
+                rightMax = max(rightMax, height[right]);
+                //if current height is lesser than the right maximum height, then we can store water, here we are storing water.
+                // else
+                res += rightMax - height[right];
                 right--;
             }
         }
