dba02e7eb76f5d53ee724df5df3354382588d704
Aditya Chaudhary <achaudhary.chaudhary991@gmail.com>
2022-01-03 16:04:04 +0530

    Time: 12 ms (57.30%), Space: 13 MB (50.14%) - LeetHub

--- word-break/word-break.cpp
+++ word-break/word-break.cpp
@@ -1,6 +1,5 @@
 class Solution {
-
-    public:
+public:
     bool _wordBreak(string s, vector<string>& wordDict) {
         int sz = s.size();
         vector<bool> dp(sz + 1, false);
@@ -8,17 +7,19 @@ class Solution {
         
         for(int i = 1; i <= sz; i++) {
             for(int j = 0; j < i; j++) {
-                 if ((dp[j]) && (find(wordDict.begin(), wordDict.end(), s.substr(j, i-j)) != wordDict.end())) {
-                    dp[i] = true; 
+                if ((dp[j]) && (find(wordDict.begin(), wordDict.end(), s.substr(j, i - j)) != wordDict.end())){
+                    dp[i] = true;
                     break;
                 }
             }
         }
         return dp.back();
-     }
+    }   
+    
+    
     
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        return _wordBreak(s,wordDict);
+        return _wordBreak(s, wordDict);
     }
 };
