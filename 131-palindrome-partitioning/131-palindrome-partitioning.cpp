75573f77de1d0b3a94ab4244ee4b823d2257641f
Aditya Chaudhary <achaudhary.chaudhary991@gmail.com>
2022-01-04 17:50:05 +0530

    Time: 172 ms (36.36%), Space: 57.6 MB (53.08%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -3,11 +3,11 @@ public:
     vector<vector<string>> partition(string s) {
         vector<vector<string> > res;
         vector<string> path;
-        func(0, s, path, res);
+        _parition(0, s, path, res);
         return res;
     }
     
-    void func(int index, string s, vector<string> &path, 
+    void _parition(int index, string s, vector<string> &path, 
               vector<vector<string> > &res) {
         if(index == s.size()) {
             res.push_back(path);
@@ -16,7 +16,7 @@ public:
         for(int i = index; i < s.size(); ++i) {
             if(isPalindrome(s, index, i)) {
                 path.push_back(s.substr(index, i - index + 1));
-                func(i+1, s, path, res);
+                _parition(i+1, s, path, res);
                 path.pop_back();
             }
         }
