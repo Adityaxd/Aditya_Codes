b27c8fc92e4f2379de156013b50d28d61a6d7e89
Aditya Chaudhary <achaudhary.chaudhary991@gmail.com>
2022-01-06 15:41:51 +0530

    Time: 38 ms (5.16%), Space: 22.4 MB (11.51%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -8,7 +8,20 @@ public:
         return res;
     }
 public:
+    int _singleNonDuplicateBinarySearch(vector<int>& nums) {
+        int low = 0, high = nums.size() - 2;
+        
+        while(low <= high) {
+            int mid = (low + high) >> 1;
+            
+            if(nums[mid] == nums[mid ^ 1]) low = mid + 1;
+            else high = mid - 1;
+        }
+        return nums[low];
+    }
+public:
     int singleNonDuplicate(vector<int>& nums) {
-        return _singleNonDuplicate(nums);
+        // return _singleNonDuplicate(nums);
+        return _singleNonDuplicateBinarySearch(nums);
     }
 };
