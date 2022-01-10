8b9f5f091cf6acc576a0e47ac25f2d8ae7f622d5
Aditya Chaudhary <achaudhary.chaudhary991@gmail.com>
2022-01-10 23:21:39 +0530

    Time: 23 ms (43.07%), Space: 7 MB (88.80%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -23,7 +23,23 @@ public:
         return dp[coinsize][amount];
     }
 public:
+    int change1D(int amount, vector<int>& coins) {
+        
+       vector<int> dp (amount + 1);
+        dp[0] = 1;
+        
+        for(int i = 0; i < coins.size(); i++) { // loop for the coins given, i ko pay karne ke kitne tareeke hai matlab agar 2 3 5 coins given hai toh pehele 2 ke tareeke then 3 ke then 5 ke.
+            for(int j = coins[i]; j < dp.size(); j++) { 
+                // j chalega iske liye ki amount kitne tareeke me pay kar sakte hai jaise for coin = 2, 7 ko 7 - 2 => 5 ke jitne tareeke hai pay karne ke uske peeche ek 2 ka coin laga denge toh saare tareeke mil jayenge using coin = 2, 7 pay karne ke.
+                dp[j] += dp[j - coins[i]]; //J - coins[i] ko pay karne ke jitne tareeke hai wo add kardenge dp[i] me, jaise 7 calculate kiya toh pehele coin = 2 ke liye dp[5] add hua tha, fir coin = 3 ke liye dp[4] add hua tha, fir coin 5 ke liye dp[2] add hua tha. Jitne tareeke me 5 pay kar sakte hai toh unke peeche agar ek 2 ka coin laga diya jaaye toh wo 7 ko pay karne ka tareeke hojayega na.
+            }
+        }
+        
+        return dp[amount];
+    }
+public:
     int change(int amount, vector<int>& coins) {
-        return _change(amount,coins);
+        // return _change(amount,coins);
+        return change1D(amount,coins);
     }
 };
